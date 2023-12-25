f52b96f4bbf2ae944a7f5a295b887c01057bf188
Lauro Stephan <110504296+steph4nn@users.noreply.github.com>
2023-12-25 20:47:30 -0300

    Adicionando projetos já realizados.

---
+++ c#-fundamentals/Calculator/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Calculator;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Menu();
+    }
+    static float Soma(float x, float y){
+        return x+y;
+    }
+    static float Subtracao(float x, float y){
+        return x-y;
+    }
+    static float Multiplicacao(float x, float y){
+        return x*y;
+    }
+    static float Divisao(float x, float y){
+        return x/y;
+    }
+    static void Menu(){
+        Console.Write(@"Qual operação deseja realizar: 
+            
+        1 - Adição
+        2 - Subtração
+        3 - Multiplicação
+        4 - Divisão
+        
+        Digite sua opção: ");
+        int x = int.Parse(Console.ReadLine());
+
+    }
+}
---
+++ c#-fundamentals/dioExemplo/Program.cs
@@ -0,0 +1,14 @@
+﻿using dioExemplo.Models;
+namespace dioExemplo;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Pessoa p = new Pessoa();
+        p.Idade = 20;
+        p.Nome = "Lauro";
+        p.Apresentar();
+        
+    }
+}
