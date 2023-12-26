7b8271c6e6e0b0ad8140b85d1ff4fdd7b2c33799
steph4nn <laurostephan@gmail.com>
2023-12-26 10:54:32 -0300

    Uso do switch

--- c#-fundamentals/dioExemplo/Program.cs
+++ c#-fundamentals/dioExemplo/Program.cs
@@ -7,65 +7,22 @@ class Program
     {
 
 
+        Console.WriteLine("Digite uma letra: ");
+        string letra = Console.ReadLine();
 
-        string a = "15";
-        int b = 5;
-
-
-        // Caso o TryParse não seja bem sucedido permanece o valor original/nulo caso a variavel a esteja sendo inicializada
-        int.TryParse(a, out b);
-        int.TryParse(a, out int c); 
-
-        Console.WriteLine(b);
-        Console.WriteLine(c);
-
-
-
-        // int inteiro = 5;
-        // string teste = inteiro.ToString();
-
-
-        // int a = Convert.ToInt32("5"); // convert aceita nulos
-        // int b = int.Parse("7"); //parse não aceita nulos
+        switch (letra){
+            
+            case "a":
+            case "e":
+            case "i":
+            case "o":
+            case "u":
+                Console.WriteLine("Vogal");
+                break;
+            default:
+                Console.WriteLine("Não é uma vogal");
+                break;
         
-        // Console.WriteLine(a);
-        // Console.WriteLine(b);
-
-
-
-
-
-        // DateTime dataAtual= DateTime.Now.AddDays(5);
-        // Console.WriteLine(dataAtual.ToString("MM/dd/yy"));
-        // Console.WriteLine(dataAtual.ToString("dd/MM/yyyy"));
-        // Console.WriteLine(dataAtual.ToString("HH:mm"));
-
-
-
-
-
-
-
-
-
-
-
-
-        // string frase = "Frase de teste";
-        // int inteiro = 777;
-        // double desconsideraZero = 1.90;
-        // decimal usadoParaDinheiro = 2.6M;
-        // float lembrarDoEfiNoFinal =  2.55F;
-        // bool booleano = true;
-
-
-        // Console.WriteLine($"Valor da váriavel: {frase}");
-        // Console.WriteLine($"Valor da váriavel: {inteiro}");
-        // Console.WriteLine($"Valor da váriavel: {desconsideraZero}");
-        // Console.WriteLine($"Valor da váriavel: {usadoParaDinheiro}");
-        // Console.WriteLine($"Valor da váriavel: {lembrarDoEfiNoFinal}");
-        // Console.WriteLine($"Valor da váriavel: {booleano}");
-
-        
+    }
     }
 }
