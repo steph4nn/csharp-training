43bf75422b2537399aef6a480f907628857e344b
steph4nn <laurostephan@gmail.com>
2023-12-25 21:20:11 -0300

    Tipo DateTime

--- c#-fundamentals/dioExemplo/Program.cs
+++ c#-fundamentals/dioExemplo/Program.cs
@@ -5,20 +5,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        string frase = "Frase de teste";
-        int inteiro = 777;
-        double desconsideraZero = 1.90;
-        decimal usadoParaDinheiro = 2.6M;
-        float lembrarDoEfiNoFinal =  2.55F;
-        bool booleano = true;
+
+        DateTime dataAtual= DateTime.Now.AddDays(5);
+        Console.WriteLine(dataAtual.ToString("MM/dd/yy"));
+        Console.WriteLine(dataAtual.ToString("dd/MM/yyyy"));
+        Console.WriteLine(dataAtual.ToString("HH:mm"));
 
 
-        Console.WriteLine($"Valor da váriavel: {frase}");
-        Console.WriteLine($"Valor da váriavel: {inteiro}");
-        Console.WriteLine($"Valor da váriavel: {desconsideraZero}");
-        Console.WriteLine($"Valor da váriavel: {usadoParaDinheiro}");
-        Console.WriteLine($"Valor da váriavel: {lembrarDoEfiNoFinal}");
-        Console.WriteLine($"Valor da váriavel: {booleano}");
+
+
+
+
+
+
+
+
+
+
+        // string frase = "Frase de teste";
+        // int inteiro = 777;
+        // double desconsideraZero = 1.90;
+        // decimal usadoParaDinheiro = 2.6M;
+        // float lembrarDoEfiNoFinal =  2.55F;
+        // bool booleano = true;
+
+
+        // Console.WriteLine($"Valor da váriavel: {frase}");
+        // Console.WriteLine($"Valor da váriavel: {inteiro}");
+        // Console.WriteLine($"Valor da váriavel: {desconsideraZero}");
+        // Console.WriteLine($"Valor da váriavel: {usadoParaDinheiro}");
+        // Console.WriteLine($"Valor da váriavel: {lembrarDoEfiNoFinal}");
+        // Console.WriteLine($"Valor da váriavel: {booleano}");
 
         
     }
