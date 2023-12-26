80af9899bc5e5326247d0f14fd9035fe04c52af8
steph4nn <laurostephan@gmail.com>
2023-12-25 21:02:28 -0300

    Declaração de tipos

--- c#-fundamentals/dioExemplo/Program.cs
+++ c#-fundamentals/dioExemplo/Program.cs
@@ -5,10 +5,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Pessoa p = new Pessoa();
-        p.Idade = 20;
-        p.Nome = "Lauro";
-        p.Apresentar();
+        string frase = "Frase de teste";
+        int inteiro = 777;
+        double desconsideraZero = 1.90;
+        decimal usadoParaDinheiro = 2.6M;
+        float lembrarDoEfiNoFinal =  2.55F;
+        bool booleano = true;
+
+
+        Console.WriteLine($"Valor da váriavel: {frase}");
+        Console.WriteLine($"Valor da váriavel: {inteiro}");
+        Console.WriteLine($"Valor da váriavel: {desconsideraZero}");
+        Console.WriteLine($"Valor da váriavel: {usadoParaDinheiro}");
+        Console.WriteLine($"Valor da váriavel: {lembrarDoEfiNoFinal}");
+        Console.WriteLine($"Valor da váriavel: {booleano}");
+
         
     }
 }
