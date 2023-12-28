00d0f04717861b8b862d13017b360e59fff4e0be
steph4nn <laurostephan@gmail.com>
2023-12-28 17:14:37 -0300

    Novo projeto e validações no GET e SET

---
+++ exemploExplorando/Program.cs
@@ -0,0 +1,14 @@
+﻿using exemploExplorando.models;
+namespace exemploExplorando;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Pessoa p1 = new Pessoa();
+        p1.Nome = "lauro";
+        p1.Idade = 21;
+        p1.Apresentar();
+    }
+}
---
+++ exemploExplorando/models/Pessoa.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace exemploExplorando.models
+{
+    public class Pessoa
+    {
+        private string _nome;
+        public string Nome 
+        { 
+            get
+            {
+                return _nome.ToUpper();
+            }
+            set
+            {
+                if (value == ""){
+                    throw new ArgumentException("O nome não pode ser vazio");
+                }
+                _nome = value;
+            }
+        }
+        public int Idade { get; set; }
+
+        public void Apresentar(){
+            Console.WriteLine($"Nome: {Nome}, Idade: {Idade}");
+        }
+    }
+}
