c3ec0b45d199f9c789e0ff5c50257e4d248cac07
steph4nn <laurostephan@gmail.com>
2023-12-28 15:54:34 -0300

    Copiando um array para o outro

--- c#-fundamentals/dioExemplo/Program.cs
+++ c#-fundamentals/dioExemplo/Program.cs
@@ -7,14 +7,17 @@ class Program
     {
         double[] arrayPotencia = new double[10];
 
-        Array.Resize(ref arrayPotencia, arrayPotencia.Length *2);
-        
+        Array.Resize(ref arrayPotencia, arrayPotencia.Length *2);     
         Console.WriteLine("Digite um número para verificar suas potencias: ");
         int.TryParse(Console.ReadLine(), out int num);
         for(int i = 0; i < arrayPotencia.Length; i++){
             arrayPotencia[i] = Math.Round(Math.Pow(num,i+1),2);
         }
-        foreach(double valor in arrayPotencia ){
+        double[] novoArray = new double [arrayPotencia.Length];
+        
+        Array.Copy(arrayPotencia, novoArray, arrayPotencia.Length);
+        
+        foreach(double valor in novoArray ){
             Console.WriteLine($"{valor}");
         }
 
