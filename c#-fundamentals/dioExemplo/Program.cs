a3f0068e8e9efde45da811a3d48b5a4b1cc27376
steph4nn <laurostephan@gmail.com>
2023-12-29 18:56:31 -0300

    Utilizando Pilha e Fila

--- c#-fundamentals/dioExemplo/Program.cs
+++ c#-fundamentals/dioExemplo/Program.cs
@@ -9,27 +9,37 @@ namespace dioExemplo
     {
         static void Main(string[] args)
         {
-            try{
-                string[] conteudoLinha = File.ReadAllLines(@"Arquivo/arq_Text.txt"); // Replace the incorrect escape character with @
+            Queue<int> fila = new Queue<int>();
 
-                foreach (string linha in conteudoLinha)
-                {
-                    Console.WriteLine(linha);
-                }
+            fila.Enqueue(2);
+            fila.Enqueue(4);
+            fila.Enqueue(6);
+            fila.Enqueue(8);
+            fila.Dequeue();
+            fila.Enqueue(10);
+
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine("---------ELEMENTOS DA FILA-----------");
+            Console.WriteLine("-------------------------------------");
+            foreach (int num in fila){
+                Console.WriteLine(num);
             }
-            
-            catch(FileNotFoundException ex){
-                Console.WriteLine($"O arquivo não foi encontrada. {ex.Message}");
+
+            Stack<int> pilha = new Stack<int>();
+            pilha.Push(2);
+            pilha.Push(4);
+            pilha.Push(6);
+            pilha.Push(8);
+            pilha.Pop();
+            pilha.Push(10);
+
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine("---------ELEMENTOS DA PILHA-----------");
+            Console.WriteLine("-------------------------------------");
+            foreach (int num in pilha){
+                Console.WriteLine(num);
             }
-            catch(DirectoryNotFoundException ex){
-                Console.WriteLine($"A pasta não foi encontrada. {ex.Message}");
-            }
-            catch(Exception ex){
-                Console.WriteLine($"Ocorreu uma exceção genérica. {ex.Message}");
-            }
-            finally {
-                Console.WriteLine("Passou por todas as exceções.");
-            }
+
         }
     }
 }
