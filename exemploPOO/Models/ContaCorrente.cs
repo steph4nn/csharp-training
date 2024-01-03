ede4e13409706a128edf1b533c1efe61847113bd
steph4nn <laurostephan@gmail.com>
2024-01-03 14:41:57 -0300

    Encapsulamento

---
+++ exemploPOO/Models/ContaCorrente.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace exemploPOO.Models
+{
+    public class ContaCorrente
+    {
+        public ContaCorrente(int numeroConta, decimal saldoInicial)
+        {
+            NumeroConta = numeroConta;
+            Saldo = saldoInicial;
+        }
+
+        public int NumeroConta { get; set; }
+        private decimal Saldo;
+
+        public void Sacar(decimal valor){
+            if (valor<=Saldo){
+                Saldo -= valor;
+                Console.WriteLine("Saque realizado com sucesso");
+            }
+            else{
+                Console.WriteLine("Valor para saque maior que o disponivel");
+            }
+
+        }
+        public void ExibirSaldo(){
+            Console.WriteLine($"Seu saldo disponivel é {Saldo}");
+        }
+    }
+}
---
+++ exemploPOO/Program.cs
@@ -0,0 +1,15 @@
+﻿using exemploPOO.Models;
+namespace exemploPOO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ContaCorrente c1 = new ContaCorrente(123,2000);
+        c1.ExibirSaldo();
+        c1.Sacar(500M);
+        c1.ExibirSaldo();
+        c1.Sacar(2000);
+        c1.ExibirSaldo();
+    }
+}
