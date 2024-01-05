9730bb6560fabf32d3a2c4f897ed2d81d20a6511
steph4nn <laurostephan@gmail.com>
2024-01-05 11:13:03 -0300

    Interface e metodos padrao

--- exemploPOO/Models/Professor.cs
+++ exemploPOO/Models/Professor.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace exemploPOO.Models
 {
-    public sealed class Professor : Pessoa
+    public class Professor : Pessoa
     {
         public decimal Salario { get; set; }
 
--- exemploPOO/Program.cs
+++ exemploPOO/Program.cs
@@ -1,16 +1,12 @@
-﻿using exemploPOO.Models;
+﻿using exemploPOO.Interfaces;
+using exemploPOO.Models;
 namespace exemploPOO;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Aluno a1 = new Aluno("Lauro", 20);
-        Professor p1 = new Professor("Alex", 50);
-        Pessoa pe1 = new Pessoa("Marrie", 3);
-
-        a1.Apresentar();
-        p1.Apresentar();
-        pe1.Apresentar();
+        Calculadora calc = new Calculadora();
+        ICalculadora calcI = new Calculadora();
     }
 }
